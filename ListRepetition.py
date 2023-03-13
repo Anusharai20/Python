f9529cc5fcaea7989493ce1094d2ea35ac63aa5c
Anusha Rai <118553742+Anusharai20@users.noreply.github.com>
2023-03-13 21:09:40 +0545

    Add files via upload

---
+++ ListRepetition.py
@@ -0,0 +1,6 @@
+'''Algorithm
+Declare a list
+multiply the list with the n where n is the number of times you want the elements of the list to be repeated'''
+L1 = [13,141,2,5,53,6,88,9] # =>Declaring the list
+R = L1*3 # => Repetition Operator
+print(R)
